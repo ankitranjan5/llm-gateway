2cdeba440b903a3fe3c1bc13b38767a0a87b9c17
Ankit Ranjan <ankit.rishu06@gmail.com>
2025-12-27 23:29:30 +0530

    first-commit

---
+++ src/test/java/com/llm/gateway/llm_gateway/LlmGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llm.gateway.llm_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
